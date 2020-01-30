7bbc628c82090f48e5d3d42b6bb0c458ccc109e7
Jean Pierre Cimalando <jp-dev@inbox.ru>
2020-01-30 03:37:27 +0100

    Doc comments

--- src/SfzFilter.h
+++ src/SfzFilter.h
@@ -3,19 +3,51 @@
 
 enum SfzFilterType : int;
 
+/**
+   Multi-mode filter for SFZ v2
+   Available for mono and stereo. (NCh=1, NCh=2)
+ */
 template <unsigned NCh>
 class SfzFilter {
 public:
     SfzFilter();
     ~SfzFilter();
 
+    /**
+       Set up the filter constants.
+       Run it exactly once after instantiating.
+     */
     void init(double sampleRate);
+
+    /**
+       Reinitialize the filter memory to zeros.
+     */
     void clear();
 
+    /**
+       Process one cycle of the filter without modulating cutoff or Q.
+       `cutoff` is a frequency expressed in Hz.
+       `q` is a resonance expressed in dB.
+       `in[i]` and `out[i]` may refer to identical buffers, for in-place processing
+     */
     void process(const float *const in[NCh], float *const out[NCh], float cutoff, float q, unsigned nframes);
+
+    /**
+       Process one cycle of the filter with cutoff and Q values varying over time.
+       `cutoff` is a frequency expressed in Hz.
+       `q` is a resonance expressed in dB.
+       `in[i]` and `out[i]` may refer to identical buffers, for in-place processing
+     */
     void processModulated(const float *const in[NCh], float *const out[NCh], const float *cutoff, const float *q, unsigned nframes);
 
+    /**
+       Get the type of filter.
+     */
     SfzFilterType type() const;
+
+    /**
+       Set the type of filter.
+     */
     void setType(SfzFilterType type);
 
 private:
